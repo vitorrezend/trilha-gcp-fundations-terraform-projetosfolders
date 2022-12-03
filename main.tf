4bdd25d2c12eada0a78f5ab04dc23706492bd763
Vitor Rezende <vitor.rezende11@gmail.com>
2022-12-03 13:12:54 -0300

    Desafio Organizacao Pastas terraform

--- main.tf
+++ main.tf
@@ -42,3 +42,78 @@ resource "google_project" "barberosa2-salesforce-dev" {
 #   auto_create_network=false
 #   billing_account = "01B2C4-70459E-F53446"
 # }
+
+resource "google_folder" "Comercial" {
+  display_name = "Comercial"
+  parent       = "organizations/540829645030"
+}
+
+resource "google_folder" "Mobile" {
+  display_name = "Mobile"
+  parent       = google_folder.Comercial.name
+}
+
+resource "google_folder" "ERP" {
+  display_name = "ERP"
+  parent       = google_folder.Comercial.name
+}
+
+resource "google_folder" "Desenvolvimento" {
+  display_name = "Desenvolvimento"
+  parent       = google_folder.Mobile.name
+}
+
+resource "google_folder" "Producao" {
+  display_name = "Producao"
+  parent       = google_folder.Mobile.name
+}
+
+resource "google_folder" "Desenvolvimento" {
+  display_name = "Desenvolvimento"
+  parent       = google_folder.ERP.name
+}
+
+resource "google_folder" "Producao" {
+  display_name = "Producao"
+  parent       = google_folder.ERP.name
+}
+
+
+resource "google_project" "barbero-comercial-mobile-dev" {
+  name       = "Mobile Dev"
+  project_id = "barberosa-comercial-mobile-dev"
+  folder_id  = google_folder.Desenvolvimento.name
+  auto_create_network=false
+  billing_account = "018973-A8340F-83D8E5"
+
+}
+
+resource "google_project" "barberosa-comercial-mobile-prod" {
+  name       = "Mobile Prod"
+  project_id = "barberosa-comercial-mobile-dev"
+  folder_id  = google_folder.Desenvolvimento.name
+  auto_create_network=false
+  billing_account = "018973-A8340F-83D8E5"
+
+}
+
+
+
+resource "google_project" "barbero-comercial-erp-dev" {
+  name       = "ERP Dev"
+  project_id = "barberosa-comercial-erp-dev"
+  folder_id  = google_folder.Desenvolvimento.name
+  auto_create_network=false
+  billing_account = "018973-A8340F-83D8E5"
+
+}
+
+
+resource "google_project" "barbero-comercial-erp-prod" {
+  name       = "ERP Producao"
+  project_id = "barberosa-comercial-erp-prod"
+  folder_id  = google_folder.Desenvolvimento.name
+  auto_create_network=false
+  billing_account = "018973-A8340F-83D8E5"
+
+}
